038e774ffce70813a3487cd676ea7083c27b6678
ADNY.HX.LEE <andy.hx.lee@deltaww.com>
2020-03-12 15:54:36 +0800

    Updates

---
+++ precache-manifest.319e1375f24833d8ace6d433af53b745.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e744238013dc19cf0166bf0e8b478a",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "19396ccadd7d2a7fc1ae",
+    "url": "/profile/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dde07ad74b4ba18c9e3c",
+    "url": "/profile/static/js/2.525458ca.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/profile/static/js/2.525458ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19396ccadd7d2a7fc1ae",
+    "url": "/profile/static/js/main.67f44454.chunk.js"
+  },
+  {
+    "revision": "6ead39c3c3c555445c51",
+    "url": "/profile/static/js/runtime-main.43091837.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
